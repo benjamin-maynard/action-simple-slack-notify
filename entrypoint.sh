1fac80cc7f663658c8413394013b3dded18d4ce7
benjamin-maynard <383838-github@ea.maynard.io>
2019-10-05 13:06:54 +0100

    fix channel

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,6 @@ if [[ -z "${INPUT_SLACKWEBHOOK}" ]]; then
   echo "Missing slackWebhook Input"
   exit 1
 fi
-if [[ -z "${INPUT_SLACKCHANNEL}" ]]; then
-  echo "Missing slackChannel Input"
-  exit 1
-fi
 if [[ -z "${INPUT_SLACKUSERNAME}" ]]; then
   echo "Missing slackUsername Input"
   exit 1
@@ -17,4 +13,4 @@ if [[ -z "${INPUT_SLACKMESSAGE}" ]]; then
   exit 1
 fi
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"$INPUT_SLACKMESSAGE", "channel":"INPUT_SLACKCHANNEL", "name":"$INPUT_SLACKUSERNAME"}' $INPUT_SLACKWEBHOOK
+curl -X POST -H 'Content-type: application/json' --data '{"text":"$INPUT_SLACKMESSAGE", "name":"$INPUT_SLACKUSERNAME"}' $INPUT_SLACKWEBHOOK
