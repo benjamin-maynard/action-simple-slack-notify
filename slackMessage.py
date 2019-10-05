d3bae94fcfe48e529f7c68ae5f3dfc3f3fb96608
benjamin-maynard <383838-github@ea.maynard.io>
2019-10-05 13:45:17 +0100

    Fix typo (stupid MacBook Pro Butterfly Keyboard)

--- slackMessage.py
+++ slackMessage.py
@@ -4,12 +4,12 @@ import json
 import requests
 
 # Get Environment Variables
-slack_webook    = environ.get("INPUT_SLACKWEBHOOK")
+slack_webhook    = environ.get("INPUT_SLACKWEBHOOK")
 slack_message   = environ.get("INPUT_SLACKMESSAGE")
 slack_username  = environ.get("INPUT_SLACKUSERNAME")
 
 # Validate Environment Variables
-if slack_webook == None:
+if slack_webhook == None:
     print("The slackWebhook input must be set")
     exit(1)
 
@@ -31,7 +31,7 @@ slack_payload = Message(slack_username, slack_message)
 slack_payload_json = json.dumps(slack_payload.__dict__)
 
 # Make request to Slack
-r = requests.post(url = slack_webook, data = slack_payload_json) 
+r = requests.post(url = slack_webhook, data = slack_payload_json) 
 
 if r.status_code != 200:
     print("Error sending message to Slack: " + r.text)
